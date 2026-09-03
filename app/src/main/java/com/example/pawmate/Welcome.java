3acbeca22636aa9dc4aacf49c4927e3c4183a5c5
Dipsan Dhital <dipsandhital12@gmail.com>
2026-09-03 23:06:26 +0545

    Welcome Backend

--- app/src/main/java/com/example/pawmate/Welcome.java
+++ app/src/main/java/com/example/pawmate/Welcome.java
@@ -1,6 +1,9 @@
 package com.example.pawmate;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,32 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Welcome extends AppCompatActivity {
 
+    Button btnLogin, btnCreateAc;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_welcome);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btnLogin= findViewById(R.id.btnLogin);
+        btnCreateAc= findViewById(R.id.btnCreateAc);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Welcome.this, LogIn.class);
+                startActivity(intent);
+            }
+        });
+
+
+        btnCreateAc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(Welcome.this, SignUp.class);
+                startActivity(intent);
+            }
         });
     }
 }
