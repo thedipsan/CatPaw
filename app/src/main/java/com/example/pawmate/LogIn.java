7d2c806237936c0278134c264ae06dd3466d6f53
Dipsan Dhital <dipsandhital12@gmail.com>
2026-09-04 13:42:42 +0545

    Initializing login and signup

--- app/src/main/java/com/example/pawmate/LogIn.java
+++ app/src/main/java/com/example/pawmate/LogIn.java
@@ -1,24 +1,124 @@
 package com.example.pawmate;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LogIn extends AppCompatActivity {
+
+    EditText etEmail, etPassword;
+    MaterialButton btnLogin;
+    TextView tvSignUp;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_log_in);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+       // Initialize Firebase Auth
+        mAuth= FirebaseAuth.getInstance();
+
+        // Connect with XML component
+        etEmail = findViewById(R.id.etEmail);
+        etPassword= findViewById(R.id.etPassword);
+
+        btnLogin= findViewById(R.id.btnLogin);
+        tvSignUp= findViewById(R.id.tvSignUp);
+
+
+
+        // Login Button
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginUser();
+            }
+        });
+
+
+        // Go to signup
+        tvSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LogIn.this, SignUp.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+    }
+
+    private void loginUser() {
+
+        String email= etEmail.getText().toString().trim();
+        String password= etPassword.getText().toString().trim();
+
+         // Validation
+        if(email.isEmpty()){
+            etEmail.setError("Enter your email");
+            etEmail.requestFocus();
+            return;
+        }
+        // Check Password
+        if(password.isEmpty()){
+            etPassword.setError("Enter your password");
+            etPassword.requestFocus();
+            return;
+        }
+
+        // Disable button while loggin in
+        btnLogin.setEnabled(false);
+        btnLogin.setText("Logging in...");
+
+        // Firebase Login
+        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if(task.isSuccessful()){
+                    Toast.makeText(LogIn.this, "Login Successful", Toast.LENGTH_SHORT).show();
+
+                    //Open Dashboard
+                    Intent intent = new Intent(LogIn.this, Dashboard.class);
+                    startActivity(intent);
+                    finish();
+                }else{
+                    btnLogin.setEnabled(true);
+                    btnLogin.setText("Login");
+                    String errorMessage= "Login failed";
+
+                    if(task.getException()!=null){
+                        errorMessage= task.getException().getMessage();
+                    }
+
+                    Toast.makeText(LogIn.this,errorMessage, Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
         });
     }
 }
+
+
+
--- app/src/main/java/com/example/pawmate/SignUp.java
+++ app/src/main/java/com/example/pawmate/SignUp.java
@@ -1,14 +1,35 @@
 package com.example.pawmate;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+
 public class SignUp extends AppCompatActivity {
+
+     EditText etName, etEmail, etPassword, etConfirmPassword;
+     MaterialButton btnCreateAccount;
+     TextView tvSignIn;
+     FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +37,140 @@ public class SignUp extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sign_up);
 
+
+        etName= findViewById(R.id.etName);
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        etConfirmPassword = findViewById(R.id.etConfirmPassword);
+
+        btnCreateAccount= findViewById(R.id.btnCreateAccount);
+
+        tvSignIn = findViewById(R.id.tvSignIn);
+
+        mAuth= FirebaseAuth.getInstance();
+
+
+
+      // Create Account
+       btnCreateAccount.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               createAccount(); 
+           }
+       });
+
+       // Goto Login
+       tvSignIn.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Intent intent = new Intent(SignUp.this, LogIn.class);
+
+           }
+       });
+
+    }
+
+
+
+    private void createAccount() {
+        String name= etName.getText().toString().trim();
+        String email= etEmail.getText().toString().trim();
+        String password = etPassword.getText().toString().trim();
+        String confirmPassword = etConfirmPassword.getText().toString().trim();
+
+
+          // Input Validation
+
+        // Check name inputfield is empty or not
+        if(name.isEmpty()){
+            etName.setError("Enter your name");
+            etName.requestFocus();
+            return;
+        }
+
+        // check email inputfiled is empty or not
+        if(email.isEmpty()){
+            etEmail.setError("Enter your email");
+            etEmail.requestFocus();
+            return;
+        }
+
+        //check password inputfild is empty or not
+        if(password.isEmpty()){
+            etPassword.setError("Enter your password");
+            etPassword.requestFocus();
+            return;
+        }
+
+        // check password length is greather than 6 or not
+        if(password.length()<6){
+            etPassword.setError("Password must be at least 6 characters");
+            etPassword.requestFocus();
+            return;
+        }
+
+        // check comparing password
+        if(!password.equals(confirmPassword)){
+            etConfirmPassword.setError("Password do not match");
+            etConfirmPassword.requestFocus();
+            return;
+        }
+
+
+
+       // Disable Button While Creating Account
+       btnCreateAccount.setEnabled(false);
+        btnCreateAccount.setText("Creating Account...");
+
+        // Create firebase account
+        mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                // Account Created
+                if(task.isSuccessful()){
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    if(user !=null){
+                        // Save User Name
+                        UserProfileChangeRequest profileUpdates= new UserProfileChangeRequest.Builder().setDisplayName(name).build();
+
+                        user.updateProfile(profileUpdates).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> ProfileTask) {
+
+                                if(ProfileTask.isSuccessful()){
+                                    Toast.makeText(SignUp.this, "Account created successfully!", Toast.LENGTH_SHORT).show();
+                                    //Open Dashboard
+                                    Intent intent= new Intent(SignUp.this, Dashboard.class);
+                                    startActivity(intent);
+                                    finish();
+                                }else{
+                                    btnCreateAccount.setEnabled(true);
+                                    btnCreateAccount.setText("Create Account");
+
+
+                                    Toast.makeText(SignUp.this, "Account created, but name could not be saved", Toast.LENGTH_SHORT).show();
+
+                                }
+
+
+                            }
+                        });
+                    }else{
+                        // Registeration Failed
+                        btnCreateAccount.setEnabled(true);
+                        btnCreateAccount.setText("Create Account");
+
+                        String errorMessage= "Signup failed";
+
+                        if(task.getException()!=null){
+                            errorMessage=task.getException().getMessage();
+                        }
+
+                        Toast.makeText(SignUp.this,errorMessage, Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
     }
 }
