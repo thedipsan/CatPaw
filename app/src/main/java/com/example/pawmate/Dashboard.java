f32231e52ec67361e2a1c37dc3d7266385e551a0
Dipsan Dhital <dipsandhital12@gmail.com>
2026-09-05 11:22:17 +0545

    Creating Dashboard Ui and make it functionally

--- app/src/main/java/com/example/pawmate/Dashboard.java
+++ app/src/main/java/com/example/pawmate/Dashboard.java
@@ -1,6 +1,11 @@
 package com.example.pawmate;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +13,97 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class Dashboard extends AppCompatActivity {
+
+    // Firebase Authentication
+    FirebaseAuth mAuth;
+
+    //UI Element
+    TextView tvGreeting;
+    ImageButton btnNotification;
+    BottomNavigationView bottomNavigation;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_dashboard);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Initialize Firebase
+        mAuth= FirebaseAuth.getInstance();
+
+        // Connect with XML
+        tvGreeting= findViewById(R.id.tvGreeting);
+        btnNotification= findViewById(R.id.btnNotification);
+        bottomNavigation= findViewById(R.id.bottomNavigation);
+
+        // Checked whether the Useer is loggedin
+        checkUser();
+
+        // Notification button
+        btnNotification.setOnClickListener(v -> {
+
+            Toast.makeText(Dashboard.this, "No new notifications", Toast.LENGTH_SHORT).show();
+
         });
+
+
+        // Bottom Navigation
+        bottomNavigation.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if(id == R.id.navHome){
+                // Already on dashboard
+                return true;
+            }
+            if(id == R.id.navPets){
+                // Open my pets
+
+                Intent intent = new Intent(Dashboard.this, MyPet.class);
+                startActivity(intent);
+                return true;
+            }
+            if(id == R.id.navSchedule){
+                //Open My Schedule
+                Intent intent= new Intent(Dashboard.this, Schedule.class);
+                startActivity(intent);
+                return true;
+            }
+            if(id== R.id.navProfile){
+                //Open My Profile
+                Intent intent = new Intent(Dashboard.this,MyProfile.class);
+                startActivity(intent);
+                return true;
+
+            }
+            return false;
+        });
+    }
+
+
+    // Check the current user inside the firebase
+    private void checkUser() {
+        FirebaseUser user = mAuth.getCurrentUser();
+        if(user == null){
+            // User is not  loggedin
+            // Send them back to log in
+            Intent intent = new Intent(Dashboard.this, LogIn.class);
+            startActivity(intent);
+            finish();
+            return;
+
+        }
+
+        // Get User's Display Name
+        String name = user.getDisplayName();
+        if(name != null && !name.isEmpty()){
+            tvGreeting.setText("Hi,"+name+"👋");
+        }else{
+            tvGreeting.setText("Hi, PawMate User");
+        }
     }
 }
